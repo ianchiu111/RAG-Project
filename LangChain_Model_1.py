48052dad938b737292d797af3fa9f6419771972d
Ian, Chiu <97895216+ianchiu111@users.noreply.github.com>
2025-01-19 01:49:57 +0800

    Add files via upload

---
+++ LangChain_Model_1.py
@@ -0,0 +1,97 @@
+# Reference：https://smith.langchain.com/onboarding?organizationId=07acc26b-a455-4c66-8557-971fd98e30c4&step=1
+# Topic：Build a simple LLM application with prompt templates and chat models
+
+import os
+import time
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
+
+# Change false to true when needed
+os.environ["LANGSMITH_TRACING"] = "false" 
+LANGSMITH_API_KEY = os.getenv('LANGSMITH_API_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# Openai api with Langchain Framework
+# setup model's parameter, like Temperature, N, top_p, etc
+llm = ChatOpenAI(
+    model = "gpt-4-turbo",
+    temperature = 1.2,
+    max_tokens = 20,
+)
+
+## Message Types： SystemMessage, HumanMessage
+messages = [
+    SystemMessage("Start a dialogue with me"),
+    HumanMessage("Introduce Tainan's weather."),
+]
+
+response = llm.invoke(messages)
+
+# Define Streaming Function
+def stream_print_out(response):
+    for chunk in response:
+        print(chunk, end="", flush=True)
+        time.sleep(0.05)  # 控制輸出速度
+
+'''  Generation Part
+# Comparasion about the response
+## General Print Out
+print("Response Content：", response.content)
+## Streaming Print Out
+stream_print_out(response.content)
+'''
+
+#Try to print out with Prompt Template
+
+from langchain_core.prompts import ChatPromptTemplate
+
+#system_template = "Translate the following from English into {language}"
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Translate the following from English into {language}"),
+        ("user", "{text}")
+    ]
+)
+
+# It's available to use invoke with {sentense} to make prompt completed, so we can make it flexible in application
+## example：please enter the text and language you want to tranlate into
+## user A：{language}：Chinese, {text}：Prompt Engineer.
+## user B：{language}：Italian, {text}：Hello!
+prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
+response = llm.invoke(prompt)
+
+''' Generation Part
+print(response.content)
+'''
+
+# Define tool calling
+# 尚未完成（暫時擱置）
+def add(a: int, b: int) -> int:
+    """Add two integers.
+
+    Args:
+        a: First integer
+        b: Second integer
+    """
+    return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    """Multiply two integers.
+
+    Args:
+        a: First integer
+        b: Second integer
+    """
+    return a * b
+
+tools = [add, multiply]
+
+llm_with_bind_tool = llm.bind_tools(tools)
+messages = [
+    HumanMessage("(3 * 5) + 9"),
+]
+
+response = llm.invoke(messages)
+print(response.content)
---
+++ LangChain_Model_2.py
@@ -0,0 +1,51 @@
+# Reference：https://smith.langchain.com/onboarding?organizationId=07acc26b-a455-4c66-8557-971fd98e30c4&step=1
+# Topic：Build a semantic search engine over a PDF with document loaders, embedding models, and vector stores
+
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+
+# Change false to true when needed
+os.environ["LANGSMITH_TRACING"] = "false" 
+LANGSMITH_API_KEY = os.getenv('LANGSMITH_API_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# Openai api with Langchain Framework
+# setup model's parameter, like Temperature, N, top_p, etc
+llm = ChatOpenAI(
+    model = "gpt-4-turbo",
+    temperature = 1.2,
+    max_tokens = 20,
+)
+
+# # Try to load PDF files
+from langchain_community.document_loaders import PyPDFLoader
+
+# file_path = ("C:/論文紀錄/12_The Rise of Artificial Intelligence under the Lens of Sustainability.pdf")
+# loader = PyPDFLoader(file_path)
+
+# docs = loader.load()
+# print(docs[0])
+
+# Try to load Web Pages
+import bs4
+import asyncio
+from langchain_community.document_loaders import WebBaseLoader
+
+page_url = "https://python.langchain.com/docs/how_to/chatbots_memory/"
+
+loader = WebBaseLoader(web_paths=[page_url])
+docs = []
+
+# 異步函數來處理 alazy_load
+## async for 需要在協程（由 async def 定義的函數）中運行
+async def load_documents(file_path):
+    loader = PyPDFLoader(file_path)
+    docs = []
+    async for doc in loader.alazy_load():
+        docs.append(doc)
+    return docs
+
+# 使用 asyncio.run() 執行異步函數
+docs = asyncio.run(load_documents(page_url))
+
