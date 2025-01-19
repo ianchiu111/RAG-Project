532a4da1bace841179fede18189503579c0d3c87
ianchiu111 <yqiu318@example.com>
2025-01-19 21:53:08 +0800

    LangChain_Model_1 and Note Update

--- LangChain_Model_1.py
+++ LangChain_Model_1.py
@@ -27,6 +27,10 @@ messages = [
 ]
 
 response = llm.invoke(messages)
+###
+print(type(messages))
+###
+
 
 # Define Streaming Function
 def stream_print_out(response):
@@ -42,12 +46,12 @@ print("Response Content：", response.content)
 stream_print_out(response.content)
 '''
 
-#Try to print out with Prompt Template
+#Try to print out with Prompt Template（ChatPromptTemplate.from_messages()）
 
 from langchain_core.prompts import ChatPromptTemplate
 
 #system_template = "Translate the following from English into {language}"
-prompt_template = ChatPromptTemplate.from_messages(
+prompt_template_1 = ChatPromptTemplate.from_messages(
     [
         ("system", "Translate the following from English into {language}"),
         ("user", "{text}")
@@ -58,13 +62,57 @@ prompt_template = ChatPromptTemplate.from_messages(
 ## example：please enter the text and language you want to tranlate into
 ## user A：{language}：Chinese, {text}：Prompt Engineer.
 ## user B：{language}：Italian, {text}：Hello!
-prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
+prompt = prompt_template_1.invoke({"language": "Italian", "text": "hi!"})
 response = llm.invoke(prompt)
+#print(response.content)
 
-''' Generation Part
+
+#Try to use few-shot examples with Prompt Template（ChatPromptTemplate.from_template()）
+
+prompt_template_2 = ChatPromptTemplate.from_template("Question: {question} \n {answer}")
+
+## Example sets to the formatter prompt
+examples = [
+    {
+        "question": "Who was the maternal grandfather of George Washington?",
+        "answer": 
+        """
+        Are follow up questions needed here: Yes.
+        Follow up: Who was the mother of George Washington?
+        Intermediate answer: The mother of George Washington was Mary Ball Washington.
+        Follow up: Who was the father of Mary Ball Washington?
+        Intermediate answer: The father of Mary Ball Washington was Joseph Ball.
+        So the final answer is: Joseph Ball
+        """,
+    },
+    {
+        "question": "Are both the directors of Jaws and Casino Royale from the same country?",
+        "answer": 
+        """
+        Are follow up questions needed here: Yes.
+        Follow up: Who is the director of Jaws?
+        Intermediate Answer: The director of Jaws is Steven Spielberg.
+        Follow up: Where is Steven Spielberg from?
+        Intermediate Answer: The United States.
+        Follow up: Who is the director of Casino Royale?
+        Intermediate Answer: The director of Casino Royale is Martin Campbell.
+        Follow up: Where is Martin Campbell from?
+        Intermediate Answer: New Zealand.
+        So the final answer is: No
+        """,
+    },
+]
+
+# Different way to print out 
+## Print out like a prompt and response
+prompt = prompt_template_2.invoke(examples[0])
+response = llm.invoke(prompt)
 print(response.content)
-'''
+## Print out like QA storytelling 
+print(prompt_template_2.invoke(examples[0]).to_string())
 
+
+'''
 # Define tool calling
 # 尚未完成（暫時擱置）
 def add(a: int, b: int) -> int:
@@ -95,3 +143,4 @@ messages = [
 
 response = llm.invoke(messages)
 print(response.content)
+'''
