012d515e679be3a9abf7e67976b735ad5dc990ea
ianchiu111 <yqiu318@example.com>
2025-01-29 22:11:24 +0800

    commit

--- LangChain_Build a Chat Model/Tool Calling + Agent .py
+++ LangChain_Build a Chat Model/Tool Calling + Agent .py
@@ -5,8 +5,7 @@
 import os
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.tools import tool
-from langchain.agents import initialize_agent, AgentType
+from langchain.agents import AgentExecutor, create_tool_calling_agent, tool
 
 # Change false to true when needed
 os.environ["LANGSMITH_TRACING"] = "true" 
@@ -18,16 +17,19 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 llm = ChatOpenAI(
     model = "gpt-4-turbo",
     temperature = 1.2,
+    logprobs = True # 用於產生 log probability
 )
 
 prompt_template = ChatPromptTemplate.from_messages(
     [
         ("user", "{input}"),
-        ("system", "{output}"),
+        ("placeholder", "{agent_scratchpad}")
     ]
 )
 
 # Topic 1：Function/Tool Calling / Combine with Agent（超級好用）
+## 使用 tool calling 需要結合 Agent 代理功能
+## Reference：https://python.langchain.com/api_reference/langchain/agents/langchain.agents.tool_calling_agent.base.create_tool_calling_agent.html
 @tool
 def add(a: int, b: int) -> int:
     """ 將兩數相加 """
@@ -41,21 +43,15 @@ def multiply(a: int, b: int) -> int:
 tools = [add, multiply]
 
 # -----------------------------
-#  Initialize the Agent
-# -----------------------------
-
-# from langchain.agents import initialize_agent, AgentType
-agent = initialize_agent(
-    tools=tools,
-    llm=llm,
-    agent=AgentType.OPENAI_FUNCTIONS,  # Choose appropriate agent type
-    verbose=True  # Set to True for detailed logs
-)
-# -----------------------------
+#  Initialize the Agent（以下為新版的 Agent 架構）
 #  Run the Agent with a Query
 # -----------------------------
+# from langchain.agents import AgentExecutor, create_tool_calling_agent, tool
 
-query = "What is 3 * 12? Also, what is 11 + 49?"
+agent = create_tool_calling_agent(llm, tools, prompt_template)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+agent_executor.invoke({"input": "what is the value of magic_function(3)?"})
 
 # -----------------------------
 #  Bonus：Topic 2：In Memory Cache
@@ -65,6 +61,4 @@ from langchain_core.caches import InMemoryCache
 set_llm_cache(InMemoryCache())
 # -----------------------------
 
-response = agent.run(query)
-print(f"Response: {response}")
 
